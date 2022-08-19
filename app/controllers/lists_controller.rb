d0e6be3c7d90742710bebcb5489c60445363839b
Clari Ursini <clari_ursini@hotmail.com>
2022-08-19 16:59:13 -0300

    adding cloudify photos

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -28,16 +28,16 @@ class ListsController < ApplicationController
   end
 
   def destroy
-      @list = List.find(params[:id])
-      @list.destroy
+    @list = List.find(params[:id])
+    @list.destroy
 
-      redirect_to lists_path, status: :see_other
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name, :picture)
+    params.require(:list).permit(:name, :picture, :photo)
   end
 
   def set_list
