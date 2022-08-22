65b106bb16910a671bcd79e8dc03c12a302d56ab
Clari Ursini <clari_ursini@hotmail.com>
2022-08-22 18:14:09 -0300

    Cleaning

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.list = @list
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+end
